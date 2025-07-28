006429d316ee5481d8dc550b0c07453283216df7
Dandee Galang <dandeegalang.ph@gmail.com>
2025-07-28 15:03:44 +0800

    implemented security and standard middleware

--- src/setupServer.ts
+++ src/setupServer.ts
@@ -7,6 +7,13 @@ import {
   NextFunction,
 } from "express";
 import http from "http";
+import cors from "cors";
+import helmet from "helmet";
+import hpp from "hpp";
+import cookierSession from "cookie-session";
+import compression from "compression";
+import HTTP_STATUS from "http-status-codes";
+import "express-async-errors";
 
 export class TakltyServer {
   private app: Application;
@@ -23,9 +30,32 @@ export class TakltyServer {
     this.startServer(this.app);
   }
 
-  private securityMiddleware(app: Application): void {}
+  private securityMiddleware(app: Application): void {
+    app.use(
+      cookierSession({
+        name: "session",
+        keys: ["test1", "test2"],
+        maxAge: 24 * 7 * 3600000, // 7 days in milliseconds
+        secure: false,
+      })
+    );
+    app.use(hpp());
+    app.use(helmet());
+    app.use(
+      cors({
+        origin: "*",
+        credentials: true,
+        optionsSuccessStatus: 200,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      })
+    );
+  }
 
-  private standardMiddleware(app: Application): void {}
+  private standardMiddleware(app: Application): void {
+    app.use(compression());
+    app.use(json({ limit: "50mb" }));
+    app.use(urlencoded({ extended: true, limit: "50mb" }));
+  }
 
   private routesMiddleware(app: Application): void {}
 
